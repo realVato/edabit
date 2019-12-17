3814d1bd3977225b4b35dc1d9ff82611c46c6728
realVato <44781061+realVato@users.noreply.github.com>
2019-12-17 04:55:58 +0200

    Chat Room Status

--- script.js
+++ script.js
@@ -115,3 +115,19 @@
 
 //     return reduced
 // }
+
+// Chat Room Status
+
+// function chatroomStatus(users) {
+//     if (users.length == 0) {
+//         return 'no one online'
+//     } else if (users.length == 1) {
+//         return `${users[0]} online`
+//     } else if (users.length == 2) {
+//         return `${users[0]} and ${users[1]} online`
+//     } else if (users.length > 2) {
+//         return `${users[0]}, ${users[1]} and ${users.length - 2} more online`
+//     }
+
+//     return
+// }
